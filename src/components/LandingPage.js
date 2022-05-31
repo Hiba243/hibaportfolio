93ad031b5c123793018048cda7bdbd99400aed7a
Hiba243 <hibafatima24@gmail.com>
2022-05-31 18:24:43 +0530

    modified landing page, updated resume

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -9,14 +9,14 @@ const LandingPage = () => {
             <section className="section-padding landing-page-flex">
                 <div>
                     <p className="text__bigheading font-accent">Hello, I’m Hiba Fatima</p>
-                    <p className="text__bigheading landing-page-underline font-accent">a UI/UX Designer & Front-end Web Developer</p>
+                    <p className="text__bigheading landing-page-underline font-accent">a Front-end Web Developer &#38; UI/UX Designer</p>
                 </div>
                 
-                    <p className="text__content">By day I build web applications at Deloitte, by night I’m exploring the world of UI/UX</p>
-                    <p className="text__content">I’m passionate about designing great user experience for digital products while caring about the impact technology has on our lives.</p>
+                    <p className="text__content">By day I build web applications at Deloitte, by night I’m exploring the world of UI/UX.</p>
+                    <p className="text__content">I’m passionate about designing great user experiences &#38; building the front-end of digital products.</p>
                 
 
-                <a href="#work"><p className="text__content button-underline ctabutton">View my work &darr;</p></a>
+                <a href="#work" style={{width:"fit-content"}}><p className="text__content button-underline ctabutton">View my work &darr;</p></a>
             </section>
             </ScrollAnimation>
             <hr />
